cddb3944de6be7a3623bb34a08f6d12f35a3b39c
aviv <harimon19@gmail.com>
2017-09-28 19:04:45 +0300

    reading how much time to run from json file and updating progress and result file

--- wrapper/wrapper.py
+++ wrapper/wrapper.py
@@ -1,14 +1,23 @@
 from concurrent.futures import ThreadPoolExecutor
 from ctypes import *
 from time import sleep
+import json
+
+# read input
+with open('./input.json', 'r') as data_file:
+    data = json.load(data_file)
+secondsToRun = int(data["secondToRun"])
+# is works but why ? data is supposed to be in a n inner scope...
+
+
+# open progress file:
+progress_file = open("progress.txt", "w")
+progress_file.truncate(0)
 
 
 def printprogress():
-    progress_file = open("progress.txt", "w")
-    progress_file.truncate(0)
+    progress_file.seek(0)
     progress_file.write(str(progress()))
-    progress_file.close()
-
 
 # connect to c++ library
 algodll = cdll.LoadLibrary('../libStub/cmake-build-debug/liblibStub.so')
@@ -19,15 +28,17 @@ doAlgo.restype = c_int
 
 # start the algorithm async
 pool = ThreadPoolExecutor(1)
-future = pool.submit(doAlgo, int(60))
+future = pool.submit(doAlgo, secondsToRun)
 
 while future.running():
     print(progress())
     printprogress()
-    sleep(0.5)
+    sleep(1)
 
-# last progress check
+# last progress check + progress file closeing
 print(progress())
+printprogress()
+progress_file.close()
 
 if future.done():
     file = open("output.txt", "w")
@@ -37,4 +48,9 @@ if future.done():
 
     print("result is ready : " + str(future.result()))
 
+else:
+    file = open("output.txt", "w")
+    file.truncate(0)
+    file.write("error!!")
+    file.close()
 
