6e6490aecaff58b4dd404b4b71820edf31142a1f
yehiyam <yehiyam@gmail.com>
2017-10-26 18:29:59 +0300

    add socket client to wrapper

--- wrapper/emit_test.py
+++ wrapper/emit_test.py
@@ -1,5 +1,5 @@
-from socketIO_client import SocketIO, LoggingNamespace
-
+from socketIO_client import SocketIO
+import json
 
 def on_connect():
     print('connect')
@@ -13,12 +13,29 @@ def on_reconnect():
     print('reconnect')
 
 
-def on_initialized_response(*args):
-    print('got \"initialized\" response', args)
+def on_initialize_response(*args):
+    print('got \"initialize\" response', args)
+    socketIO.emit('initialized')
 
 
-def on_started_response(*args):
-    print('got \"started\" response', args)
+def on_command(*args):
+    message = args[0]
+    command = message["command"]
+    data = message["data"]
+    print('command: ', command)
+    print('data: ', data)
+    if command == 'initialize':
+        outmessage = {'command':'initialized'}
+        socketIO.emit('commandMessage', outmessage)
+    elif command == 'start':
+        outmessage = {'command': 'started'}
+        socketIO.emit('commandMessage', outmessage)
+
+
+def on_start_response(*args):
+    print('got \"start\" response', args)
+    socketIO.emit('started')
+
 
 
 def on_finished_response(*args):
@@ -28,7 +45,8 @@ def on_finished_response(*args):
 def on_stopped_response(*args):
     print('got \"stopped\" response', args)
 
-socketIO = SocketIO('127.0.0.1', 3000, LoggingNamespace)
+
+socketIO = SocketIO('127.0.0.1', 3000)
 
 
 socketIO.on('connect', on_connect)
@@ -36,32 +54,17 @@ socketIO.on('disconnect', on_disconnect)
 socketIO.on('reconnect', on_reconnect)
 
 # Listen
-socketIO.on('initialized', on_initialized_response)
-socketIO.on('started', on_started_response)
-socketIO.on('finished', on_finished_response)
-socketIO.on('stopped', on_stopped_response)
+socketIO.on('commandMessage', on_command)
+socketIO.on('start', on_start_response)
 
-
-print("emitting initialize!")
-socketIO.emit('initialize', "initialize data")
-socketIO.wait(seconds=4)
-print("emitting start!")
-socketIO.emit('start', "start data")
-socketIO.wait(seconds=4)
-print("emitting stop!")
-socketIO.emit('stop', "stop data")
-socketIO.wait(seconds=4)
-
-'''
-
-# Stop listening
-socketIO.off('started')
-socketIO.emit('start', "start data")
-socketIO.wait(seconds=5)
-
-# Listen only once
-socketIO.once('start', on_started_response)
-socketIO.emit('start', "start data")  # Activate aaa_response
-socketIO.emit('start', "start data")  # Ignore
-socketIO.wait(seconds=5)
-'''
+socketIO.wait()
+#
+# print("emitting initialize!")
+# socketIO.emit('initialize', "initialize data")
+# socketIO.wait(seconds=4)
+# print("emitting start!")
+# socketIO.emit('start', "start data")
+# socketIO.wait(seconds=4)
+# print("emitting stop!")
+# socketIO.emit('stop', "stop data")
+# socketIO.wait(seconds=4)
--- wrapper/wrapper.py
+++ wrapper/wrapper.py
@@ -2,59 +2,121 @@ from concurrent.futures import ThreadPoolExecutor
 from ctypes import *
 from time import sleep
 import json
+import os
+from socketIO_client import SocketIO, LoggingNamespace
 
 
+def get_progress(future, progressFunc):
+    while future.running():
+        progress = progressFunc()
+        print(progress)
+        outMessage = {'command': 'progress', 'data': {'progress': progress}}
+        socketIO.emit('commandMessage', outMessage)
+        sleep(1)
+    outMessage = {'command': 'done', 'data': {'output':['out1', 'out2']}}
+    socketIO.emit('commandMessage', outMessage)
 
 
+def run_algo():
+    # connect to c++ library
+    algodll = cdll.LoadLibrary('../libStub/build/liblibStub.so')
+    progress = algodll.progress
+    progress.restype = c_double
+    doAlgo = algodll.doAlgo
+    doAlgo.restype = c_int
+    # start the algorithm async
+    pool = ThreadPoolExecutor(2)
+    future = pool.submit(doAlgo, 3)
+    progress_future = pool.submit(get_progress, future, progress)
 
-# read input
-with open('./input.json', 'r') as data_file:
-    data = json.load(data_file)
-secondsToRun = int(data["secondToRun"])
-# is works but why ? data is supposed to be in a n inner scope...
+
+def on_connect():
+    print('connect')
 
 
-# open progress file:
-progress_file = open("progress.txt", "w")
-progress_file.truncate(0)
+def on_disconnect():
+    print('disconnect')
 
 
-def printprogress():
-    progress_file.seek(0)
-    progress_file.write(str(progress()))
+def on_reconnect():
+    print('reconnect')
 
-# connect to c++ library
-algodll = cdll.LoadLibrary('../libStub/cmake-build-debug/liblibStub.so')
-progress = algodll.progress
-progress.restype = c_double
-doAlgo = algodll.doAlgo
-doAlgo.restype = c_int
 
-# start the algorithm async
-pool = ThreadPoolExecutor(1)
-future = pool.submit(doAlgo, secondsToRun)
+def on_command(*args):
+    message = args[0]
+    command = message["command"]
+    data = message["data"]
+    print('command: ', command)
+    print('data: ', data)
+    if command == 'initialize':
+        outMessage = {'command': 'initialized'}
+        socketIO.emit('commandMessage', outMessage)
+    elif command == 'start':
+        run_algo()
+        outMessage = {'command': 'started'}
+        socketIO.emit('commandMessage', outMessage)
 
-while future.running():
-    print(progress())
-    printprogress()
-    sleep(1)
 
-# last progress check + progress file closeing
-print(progress())
-printprogress()
-progress_file.close()
+socketPort = os.getenv('WORKER_SOCKET_PORT', 3000)
+socketIO = SocketIO('127.0.0.1', socketPort)
 
-if future.done():
-    file = open("output.txt", "w")
-    file.truncate(0)
-    file.write(str(future.result()))
-    file.close()
+socketIO.on('connect', on_connect)
+socketIO.on('disconnect', on_disconnect)
+socketIO.on('reconnect', on_reconnect)
 
-    print("result is ready : " + str(future.result()))
+# Listen
+socketIO.on('commandMessage', on_command)
 
-else:
-    file = open("output.txt", "w")
-    file.truncate(0)
-    file.write("error!!")
-    file.close()
-
+socketIO.wait()
+#
+# # read input
+# with open('./input.json', 'r') as data_file:
+#     data = json.load(data_file)
+# secondsToRun = int(data["secondToRun"])
+# # is works but why ? data is supposed to be in a n inner scope...
+#
+#
+# # open progress file:
+# progress_file = open("progress.txt", "w")
+# progress_file.truncate(0)
+#
+#
+# def printprogress():
+#     progress_file.seek(0)
+#     progress_file.write(str(progress()))
+#
+# # connect to c++ library
+# algodll = cdll.LoadLibrary('../libStub/build/liblibStub.so')
+# progress = algodll.progress
+# progress.restype = c_double
+# doAlgo = algodll.doAlgo
+# doAlgo.restype = c_int
+#
+# # start the algorithm async
+# pool = ThreadPoolExecutor(1)
+# future = pool.submit(doAlgo, secondsToRun)
+#
+# while future.running():
+#     print(progress())
+#     printprogress()
+#     sleep(1)
+#
+# # last progress check + progress file closeing
+# print(progress())
+# printprogress()
+# progress_file.close()
+#
+# if future.done():
+#     file = open("output.txt", "w")
+#     file.truncate(0)
+#     file.write(str(future.result()))
+#     file.close()
+#
+#     print("result is ready : " + str(future.result()))
+#
+# else:
+#     file = open("output.txt", "w")
+#     file.truncate(0)
+#     file.write("error!!")
+#     file.close()
+#
